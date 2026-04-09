9d0b4996118b4e079bada8f0a4c17ea3746e367f
DawitA <daskabe@gmail.com>
2026-04-09 14:56:21 -0500

    update links for store

--- src/config/installLinks.ts
+++ src/config/installLinks.ts
@@ -1,4 +1,4 @@
 export const installLinks = {
-  chrome: 'https://chromewebstore.google.com/',
-  vscode: 'https://marketplace.visualstudio.com/',
+  chrome: 'https://chromewebstore.google.com/detail/agently-local-prompt-to-l/cdcngpbohinmfhnpnilaijkkdloiokcl',
+  vscode: 'https://marketplace.visualstudio.com/items?itemName=bytedyno.agently-vscode-extension',
 };
